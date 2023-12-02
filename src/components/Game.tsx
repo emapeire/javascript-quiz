61e93493f9a32d72216c211e63371b2f8866b658
Emanuel Peire <emapeire@outlook.com>
2023-12-02 15:59:15 -0300

    Add react-syntax-highlighter and update package dependencies

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,13 +1,44 @@
-import { Card, IconButton, Stack, Typography } from '@mui/material'
+import {
+  Card,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Stack,
+  Typography
+} from '@mui/material'
 import { useQuestionStore } from '../store/question'
 import { type Question } from '../types'
+import SyntaxHighlighter from 'react-syntax-highlighter'
+import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 const Question = ({ info }: { info: Question }) => {
   return (
-    <Card variant='outlined'>
+    <Card variant='outlined' sx={{ textAlign: 'left', bgcolor: '#222', p: 2 }}>
       <Typography variant='h5' component='h2'>
         {info.question}
       </Typography>
+      <SyntaxHighlighter
+        language='javascript'
+        style={gradientDark}
+        customStyle={{
+          borderRadius: '8px',
+          paddingInline: '1rem',
+          paddingBlock: '2rem'
+        }}
+      >
+        {info.code}
+      </SyntaxHighlighter>
+      <List sx={{ bgcolor: '#333', borderRadius: '8px' }} disablePadding>
+        {info.answers.map((answer, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton sx={{ borderRadius: '8px' }}>
+              <ListItemText primary={answer} sx={{ textAlign: 'center' }} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
     </Card>
   )
 }
