5d12b52a0250ea2c7d869fa13f94a6fdb8b4646e
Emanuel Peire <emapeire@outlook.com>
2023-12-04 18:31:00 -0300

    Update Footer component with corrects, incorrects, and unanswered counts

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,41 @@ export default function Footer() {
 
   return (
     <footer style={{ marginTop: '2rem' }}>
-      <strong>{`✅ ${correct} corrects - ❌ ${incorrect} incorrects - ❓ ${unanswered} unanswered`}</strong>
+      <div style={{ display: 'flex', justifyContent: 'center', gap: 32 }}>
+        <p>
+          ✅ Corrects:{' '}
+          <strong
+            style={{
+              border: '1px solid gray',
+              paddingInline: 6,
+              paddingBlock: 2,
+              borderRadius: 4
+            }}
+          >{`${correct}`}</strong>
+        </p>
+        <p>
+          ❌ Incorrects:{' '}
+          <strong
+            style={{
+              border: '1px solid gray',
+              paddingInline: 6,
+              paddingBlock: 2,
+              borderRadius: 4
+            }}
+          >{`${incorrect}`}</strong>
+        </p>
+        <p>
+          ❓Unanswered:{' '}
+          <strong
+            style={{
+              border: '1px solid gray',
+              paddingInline: 6,
+              paddingBlock: 2,
+              borderRadius: 4
+            }}
+          >{`${unanswered}`}</strong>
+        </p>
+      </div>
       <div style={{ marginTop: '2rem' }}>
         <Button onClick={() => reset()}>Reset the game</Button>
       </div>
