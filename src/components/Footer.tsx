0d4c36f4047e676f6619c9732b5a99cabd3db3d4
Emanuel Peire <emapeire@outlook.com>
2023-12-04 17:19:39 -0300

    Fix import paths and uncomment code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
 import { Button } from '@mui/material'
-// import { useQuestionsData } from './hooks/useQuestionsData'
+import { useQuestionsData } from '../hooks/useQuestionsData'
 import { useQuestionsStore } from '../store/questions'
 
 export default function Footer() {
-  // const { correct, incorrect, unanswered } = useQuestionsData()
+  const { correct, incorrect, unanswered } = useQuestionsData()
   const reset = useQuestionsStore((state) => state.reset)
 
   return (
     <footer style={{ marginTop: '2rem' }}>
-      {/* <strong>{`✅ ${correct} corrects - ❌ ${incorrect} incorrects - ❓ ${unanswered} without answer`}</strong> */}
+      <strong>{`✅ ${correct} corrects - ❌ ${incorrect} incorrects - ❓ ${unanswered} without answer`}</strong>
       <div style={{ marginTop: '2rem' }}>
         <Button onClick={() => reset()}>Reset the game</Button>
       </div>
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,19 +1,19 @@
 import { Button } from '@mui/material'
-// import { useQuestionsData } from './hooks/useQuestionsData'
+import { useQuestionsData } from '../hooks/useQuestionsData'
 import { useQuestionsStore } from '../store/questions'
 
 export const Results = () => {
-  // const { correct, incorrect } = useQuestionsData()
+  const { correct, incorrect } = useQuestionsData()
   const reset = useQuestionsStore((state) => state.reset)
 
   return (
     <div style={{ marginTop: '16px' }}>
       <h1>Your score: </h1>
 
-      {/* <strong>
+      <strong>
         <p>✅ {correct} corrects</p>
         <p>❌ {incorrect} incorrects</p>
-      </strong> */}
+      </strong>
 
       <div style={{ marginTop: '16px' }}>
         <Button onClick={() => reset()}>Try again!</Button>
